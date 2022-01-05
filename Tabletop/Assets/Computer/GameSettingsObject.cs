7aa84605cd648dd723669ed07efbff27193ec0bd
Collin Gale <Collin.g086@gmail.com>
2022-01-04 23:27:55 -0700

    Basic Local Multiplayer
    Basics

---
+++ Tabletop/Assets/Computer/GameSettingsObject.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class GameSettingsObject : ScriptableObject
+{
+    public bool isTimeLimit;
+    public float timeLimit;
+    public bool teams, sevensZeroes, jumpIn, twosOnFours, blankReflect;
+}
